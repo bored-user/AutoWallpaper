a90630ea8285eb50c7ae46f00375e69c8b1d6f47
João Vaz <joao.guimaraes.alves.vaz@gmail.com>
2019-11-28 20:01:08 -0300

    Coments

--- tasks/AW-Main.sh
+++ tasks/AW-Main.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 proc=$(ps aux | grep [A]W-Main -wc)
-if [ $proc -eq 2 ]; then
+if [ $proc -eq 2 ]; then # making sure a new instance isn't started. if this happens, there will be several files changing wallpaper every minute.
     while ( true );
     do
         ~/bin/AutoWallpaper/tasks/AW-Change.sh
