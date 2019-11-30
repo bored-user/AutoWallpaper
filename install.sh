b6e0e017eca88bd17b2628aff4b2f7d02305ec1e
João Vaz <joao.guimaraes.alves.vaz@gmail.com>
2019-11-30 14:48:48 -0300

    Performance change

--- install.sh
+++ install.sh
@@ -6,4 +6,5 @@ if [[ ! -d /home/$USER/bin/AutoWallpaper ]]; then # if AW dir doesn't exist
     mkdir /home/$USER/bin/AutoWallpaper
 fi
 /bin/cp -rf * /home/$USER/bin/AutoWallpaper
+mkdir /home/$USER/bin/AutoWallpaper/trash
 exit
