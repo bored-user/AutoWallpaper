bc1e95d976c911e63e7f7affd79207a12c4d579d
João Vaz <joao.guimaraes.alves.vaz@gmail.com>
2019-11-29 21:11:21 -0300

    Add "new instance" notification and logging

--- tasks/AW-Main.sh
+++ tasks/AW-Main.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 proc=$(ps aux | grep [A]W-Main -wc)
 if [ $proc -eq 2 ]; then # making sure a new instance isn't started. if this happens, there will be several files changing wallpaper every minute.
+    notify-send --icon="/home/$USER/bin/AutoWallpaper/img/favico.png" "AutoWallpaper" "New instance started." # new instance notification
+    /home/$USER/bin/AutoWallpaper/tasks/AW-Log.sh "New instance started"
     while ( true );
     do
-        ~/bin/AutoWallpaper/tasks/AW-Change.sh
+        /home/$USER/bin/AutoWallpaper/tasks/AW-Change.sh
         sleep 60
     done
 fi
