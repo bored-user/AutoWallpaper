902b160211aa030a494db058eba0b6eb1ca7b9f1
João Vaz <joao.guimaraes.alves.vaz@gmail.com>
2019-12-03 19:28:43 -0300

    Add file to use wget in order to download favico

---
+++ tasks/AW-Image.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+cd $dir
+cd ../
+wget -qO img/favico "https://yt3.ggpht.com/a/AGF-l780UJkGtvM9qZ6dEPig75iDfJYGre5egD5ZIw=s288-c-k-c0xffffffff-no-rj-mo"
