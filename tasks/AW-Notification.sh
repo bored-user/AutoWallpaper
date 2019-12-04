a09ae2836131dc1d26d248067a34eb8d31570037
João Vaz <joao.guimaraes.alves.vaz@gmail.com>
2019-12-04 20:19:34 -0300

    Change conditional

--- tasks/AW-Notification.sh
+++ tasks/AW-Notification.sh
@@ -3,13 +3,9 @@ dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )" # Get cu
 cd $dir # Change directory to it
 cd ../ # Go one directory up (in order to get to project root)
 source settings.conf
-if [ ! -z ${1+x} ] && [ ! -z ${2+x} ]; then # $1 = Message
-    cd $2                                   # $2 = Condition
-    source settings.conf                    # $3 = Urgency (values: "urgent" or unset). 
-    if [ -z ${4+x} ]; then
-        let $4=default
-    fi
-    if ([ ${!3} = "true" ] && [ $notification = "true" ]) || [ $4 = "urgent" ]; then # If new instance fail AND notification options are true
+if [ ! -z ${1+x} ] && [ ! -z ${2+x} ]; then # $1 = Message, $2 = Condition, $3 = Urgency                           
+    source settings.conf
+    if ([ ${!2} = "true" ] && [ $notification = "true" ]) || [ $3 = "urgent" ]; then # If new instance fail AND notification options are true
         if [ -f img/favico ]; then # If file exist
             notify-send --icon="$PWD/img/favico" "AutoWallpaper" "$1" # Notify
         else
